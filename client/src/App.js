5d50962a09f2a4bc808b996311a8126d35e3f7d0
Steve McNamara <stephenmcnamara36@gmail.com>
2021-04-01 10:09:50 -0600

    added auth0 stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+import { NavAuth, FooterOC, Loading } from "./components";
+import { Home, Profile, AddActivity, Register, Rules } from "./pages";
+
+import "./app.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavAuth />
+      <div className="container flex-grow-1">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/add" component={AddActivity} />
+          <Route path="/register" component={Register} />
+          <Route path="/rules" component={Rules} />
+        </Switch>
+      </div>
+      <FooterOC />
     </div>
   );
-}
+};
 
 export default App;
