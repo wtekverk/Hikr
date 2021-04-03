9a893a143ea068e7099813a18494fdb5939a9624
Steve McNamara <stephenmcnamara36@gmail.com>
2021-04-03 10:11:01 -0600

    added content for client side communication with back-end

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,22 +1,49 @@
 import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-    try {
-        const {data} = await api.fetchPosts();
-        dispatch({type:"FETCH_ALL", payload: data})
-    } catch (error) {
-        console.log(error.message)
-    }
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPosts(post);
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
 
-export const createPost = () => async (dispatch) => {
+    dispatch({ type: "UPDATE", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-    try {
-        const {data} = await api.createPosts(post)
-        dispatch({type: "CREATE", payload:data})
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: "LIKE", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: "DELETE", payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
