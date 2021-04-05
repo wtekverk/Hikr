b3353e93dbc627b2c586b1f206cd6aac2298dde2
Wendy Tekverk <wtekverk@gmail.com>
2021-04-05 10:47:49 -0600

    changing classnames in compents

--- client/src/components/NavAuth.js
+++ client/src/components/NavAuth.js
@@ -3,17 +3,15 @@ import React from "react";
 import NavbarOC from "./NavbarOC";
 import AuthNav from "./auth0/auth-nav";
 
+
+
 const NavAuth = () => {
   return (
-    <div className="nav-container mb-3">
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <div className="container">
-          <div className="navbar-brand logo" />
+    <>
+    
           <NavbarOC />
           <AuthNav />
-        </div>
-      </nav>
-    </div>
+          </>
   );
 };
 
