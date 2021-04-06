129dc3d368f6514861e64dfab4a8bbec0d381aa1
Curtis P <curtis.pazdar@gmail.com>
2021-04-06 12:18:33 -0600

    HerokuPush4

--- server.js
+++ server.js
@@ -12,19 +12,20 @@ app.use(cors());
 app.use("/api", require("./routes/api"));
 
 // const CONNECTION_URL = "mongodb://localhost/Hikr";
-const CONNECTION_URL =
-  "mongodb+srv://curtishp04:cpsp1994!@cluster0.cg3z6.mongodb.net/Hikr?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://curtishp04:cpsp1994!@cluster0.cg3z6.mongodb.net/Hikr?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //app.use(express.static(path.join(__dirname, 'public')));
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Hikr",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
-mongoose.set("useFindAndModify", false);
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
