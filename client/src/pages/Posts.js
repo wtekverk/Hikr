fbba573ba63d731d81b394513395e1d34d143ae5
Curtis P <curtis.pazdar@gmail.com>
2021-04-06 12:24:46 -0600

    HerokuPush6

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useStoreContext as Context } from "../utils/GlobalState";
 import { LOADING, UPDATE_POSTS, SET_ERROR } from "../utils/actions";
 import API from "../utils/API";
-import { PostsList, PostsListItem } from "../components";
+import  PostsList from "../components/postsList";
+import  PostsListItem from "../components/postsList";
 import LOGO from "../assets/images/logo.png";
 
 function Posts() {
